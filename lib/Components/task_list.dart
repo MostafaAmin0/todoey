01856dbdc45d69dfe9cb3408913c45a2e2fad299
m0stafaamin <mostafaamin928@gmail.com>
2021-08-04 14:56:44 +0200

    checkState

--- lib/Components/task_list.dart
+++ lib/Components/task_list.dart
@@ -1,23 +1,54 @@
 import 'package:flutter/material.dart';
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
   const TaskList({required this.tasks});
 
   final List<String> tasks;
 
   @override
+  _TaskListState createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  bool checkState = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: tasks.length,
+      itemCount: widget.tasks.length,
       itemBuilder: (context, index) {
         return ListTile(
-          title: Text(tasks[index]),
-          trailing: Checkbox(
-            value: false,
-            onChanged: null,
+          title: Text(
+            widget.tasks[index],
+            style: TextStyle(
+              decoration: checkState ? TextDecoration.lineThrough : null,
+            ),
+          ),
+          trailing: TaskCheckbox(
+            isChecked: checkState,
+            checkboxCallback: (checkboxState) {
+              setState(() {
+                checkState = checkboxState!;
+              });
+            },
           ),
         );
       },
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  const TaskCheckbox({this.isChecked: false, required this.checkboxCallback});
+  final bool isChecked;
+  final Function(bool?)? checkboxCallback;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: checkboxCallback,
     );
   }
 }
